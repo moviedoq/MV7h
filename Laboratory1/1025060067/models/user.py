716268137d8d5b8318a358dba62c88e6d136db77
Danochoa09 <143036159+Danochoa09@users.noreply.github.com>
2025-05-29 20:55:59 -0500

    Update and rename Laboratory1/1025060067/models.py to Laboratory1/1025060067/models/user.py

--- Laboratory1/1025060067/models/user.py
+++ Laboratory1/1025060067/models/user.py
@@ -1,4 +1,3 @@
-# notification_system/models.py
 import uuid
 
 class User:
@@ -39,28 +38,3 @@ class User:
             "available_channels": self.available_channels
         }
 
-class Notification:
-    """
-    Represents a notification to be sent.
-    """
-    def __init__(self, user_name: str, message: str, priority: str):
-        if not user_name or not isinstance(user_name, str):
-            raise ValueError("User name must be a non-empty string.")
-        if not message or not isinstance(message, str):
-            raise ValueError("Message must be a non-empty string.")
-        if priority not in ["high", "medium", "low"]:
-            raise ValueError("Priority must be 'high', 'medium', or 'low'.")
-
-        self.user_name = user_name
-        self.message = message
-        self.priority = priority
-
-    def to_dict(self) -> dict:
-        """
-        Converts the Notification object to a dictionary.
-        """
-        return {
-            "user_name": self.user_name,
-            "message": self.message,
-            "priority": self.priority
-        }
